3674155fefe18145fad124a04543703f31a7cd71
Zunair Khawaja <102663927+khzunair@users.noreply.github.com>
2024-08-06 16:15:22 +0500

    Building UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,32 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Text, View, Image } from 'react-native';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import Colors from '../constants/Colors';
+
+import products from '@/assets/data/products';
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text style={styles.title}>Hello Zunair</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image }} style={styles.image} />
+    <Text style={styles.title}>Pizza Pepporoni</Text>
+    <Text style={styles.price}>$20</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius:20
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+     fontWeight:"600",
+     marginVertical: 10
+    },
+    image:{
+      //  width: 200,
+      //   height: 200 
+      aspectRatio: 1,
+      width: '100%',
+      },
+  price:{
+    color: Colors.light.tint,
+  }
 });
